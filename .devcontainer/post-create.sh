aed6f9affdf1b065af823ccdc353d27d2449679e
Jasper <jjasper.jonker@gmail.com>
2024-12-17 15:37:22 +0000

    Disable pre-commit for the moment
    Signed-off-by: Jasper <jjasper.jonker@gmail.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,22 +8,22 @@ pyenv global zephyr-dev
 python --version
 
 # Update west project
-west update
+# west update
 
 # Install zephyr dependencies
 pip install --upgrade pip
-pip install -r zephyr/scripts/requirements.txt
-pip install -r bootloader/mcuboot/scripts/requirements.txt
+# pip install -r zephyr/scripts/requirements.txt
+# pip install -r bootloader/mcuboot/scripts/requirements.txt
 
 # install pre-commit
-pip install --upgrade pre-commit
+# pip install --upgrade pre-commit
 
-pre-commit install
+# pre-commit install
 
-pre-commit run --files app/west.yml
+# pre-commit run --files app/west.yml
 
-# Install zephyr j-link plugin
-cd modules/debug/jlink-zephyr
-mkdir build && cd build
-cmake ..
-cmake --build .
+# # Install zephyr j-link plugin
+# cd modules/debug/jlink-zephyr
+# mkdir build && cd build
+# cmake ..
+# cmake --build .
